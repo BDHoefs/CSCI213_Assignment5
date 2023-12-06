1bc6655c559cce60ba970027932cdb513e52a282
kaitlyn-nickel <nickelk102703@gmail.com>
2023-12-05 21:24:44 -0600

    Added the Log in and edited the home page

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Assignment5.Controllers
+{
+    public class LoginController : Controller
+    {
+        MusicStoreContext context;
+        public LoginController(MusicStoreContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SetSession()
+        {
+            HttpContext.Session.SetInt32("userId", 1);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Login(string userName, string password)
+        {
+            var user = (from x in context.Users where x.UserName == userName select x).First();
+            if (user.Password != password)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            HttpContext.Session.SetInt32("userId", user.UserId);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
